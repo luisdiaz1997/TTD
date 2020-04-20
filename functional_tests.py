368c0e7547ef89c1c3dd0eacef95c7ee560a0d7d
luisdiaz1997 <luisfcd2013@hotmail.com>
2020-04-19 19:49:47 -0700

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -9,10 +11,42 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-
+        
+        #She goes to the homepage
         self.browser.get('http://localhost:8000')
+
+        #She notices that the title and header contain To-Do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
+        #She is invited to prompt a To-Do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual
+        (
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
+
+        #She types "Buy peacock feathers" into a text box
+        inputbox.send_keys('Buy peacock feathers')
+
+        #When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in the to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue
+        (
+                any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+        
+        #There is still a text box inviting her to add another item,
+        #She enters "Use peacock feathers to make a fly"
         self.fail('Finishing the Test!')
+        #The page updates again and now shows both items on her list
 
 if __name__ == '__main__':
     unittest.main(warnings = 'ignore')
